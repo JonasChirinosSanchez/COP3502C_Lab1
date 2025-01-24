d3a0e6634ca787e3d83f8efa2fda6f8353500fa9
JonasChirinosSanchez <jonas89jonas89@gmail.com>
2025-01-24 10:46:45 -0500

    Update Lab1_B.py

--- Lab1_B.py
+++ Lab1_B.py
@@ -3,4 +3,4 @@ item_price = float(input("Enter the price of the item: "))
 sales_tax_percentage = float(input("Enter the sales tax percentage: "))
 
 total_price = round(item_price + (sales_tax_percentage * .01) * item_price, 2)
-print(f"Your total is ${total_price}")
+print(f"Your total is ${total_price:.2f}")
